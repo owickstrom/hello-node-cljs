b5d37e1c3edd3f5d24247f8441cfc30ee6c4a715
Oskar Wickström <oskar.wickstrom@live.com>
2014-03-01 13:42:55 +0100

    Use lein-npm instead of npm.

--- project.clj
+++ project.clj
@@ -3,10 +3,17 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2173"]
                  [hiccups "0.3.0"]]
-  :plugins [[lein-cljsbuild "1.0.2"]]
+
+  :node-dependencies [[express "latest"]]
+  :nodejs {:scripts {:start "node server"}}
+
+  :plugins [[lein-cljsbuild "1.0.2"]
+            [lein-npm "0.3.0"]]
+
   :source-paths ["src"]
   :cljsbuild {:builds [{:id "server"
                         :source-paths ["src"]
